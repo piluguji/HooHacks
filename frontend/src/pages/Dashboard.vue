c6b42dda0d8561e5ee58397e58a2430a1d369acd
Shreepa Parthaje <shreepa.parthaje@gmail.com>
2022-03-27 03:13:08 -0400

    e

--- frontend/src/pages/Dashboard.vue
+++ frontend/src/pages/Dashboard.vue
@@ -2,7 +2,8 @@
   <div class="content">
     <div class="md-layout">
       <div
-        class="md-layout-item md-medium-size-100 md-xsmall-size-100 md-size-33"
+        class="md-layout-item md-medium-size-100 md-xsmall-size-100 md-size-95"
+        style="margin-left:10px;"
       >
         <chart-card
           :chart-data="dailySalesChart.data"
@@ -11,52 +12,27 @@
           data-background-color="green"
         >
           <template slot="content">
-            <h4 class="title">1 Week View</h4>
+            <h4 id="title">1 Week View</h4>
+            <br/>
+            <span class="bt" v-on:click="oneweek">1 Week View</span>
+             <span class="bt">1 Month View</span>
+              <span class="bt">1 Year View</span>
           </template>
         </chart-card>
       </div>
-      <div
-        class="md-layout-item md-medium-size-100 md-xsmall-size-100 md-size-33"
-      >
-        <chart-card
-          :chart-data="dailySalesChart.data"
-          :chart-options="dailySalesChart.options"
-          :chart-type="'Line'"
-          data-background-color="green"
-        >
-          <template slot="content">
-            <h4 class="title">1 Month View</h4>
-          </template>
 
-        </chart-card>
-      </div>
       <div
-        class="md-layout-item md-medium-size-100 md-xsmall-size-100 md-size-33"
-      >
-        <chart-card
-          :chart-data="dailySalesChart.data"
-          :chart-options="dailySalesChart.options"
-          :chart-type="'Line'"
-          data-background-color="green"
-        >
-          <template slot="content">
-            <h4 class="title">1 Year View</h4>
-          </template>
-
-        </chart-card>
-      </div>
-    
-      <div
-        class="md-layout-item md-medium-size-50 md-xsmall-size-100 md-size-33"
+        class="x md-layout-item md-medium-size-50 md-xsmall-size-100 md-size-30"
+        style="margin-left: 10px;"
       >
         <stats-card data-background-color="red">
           <template slot="header">
             <md-icon>info_outline</md-icon>
           </template>
 
-          <template slot="content">
-            <b><p class="category" style="text-color:blue;font-size:20px;">Risk Index</p></b>
-            <h3 class="title">75</h3>
+          <template slot="content"><br/>
+            <b><p class="category" style="text-color:blue;font-size:40px;">Risk Index</p></b><br/>
+            <h3 class="title">{{this.riskNumber}}</h3>
           </template>
 
         </stats-card>
@@ -64,32 +40,34 @@
 
 
       <div
-        class="md-layout-item md-medium-size-50 md-xsmall-size-100 md-size-33"
+        class="x md-layout-item md-medium-size-50 md-xsmall-size-100 md-size-30"
+        style="margin-left: 50px;"
       >
         <stats-card data-background-color="red">
           <template slot="header">
             <md-icon>store</md-icon>
           </template>
 
-          <template slot="content">
-            <b><p class="category" style="text-color:blue;font-size:20px;">Confidence Index</p></b>
-            <h3 class="title">75</h3>
+           <template slot="content"><br/>
+            <b><p class="category" style="text-color:blue;font-size:40px;">Confidence Index</p></b><br/>
+            <h3 class="title">{{this.confidenceNumber}}</h3>
           </template>
 
         </stats-card>
       </div>
 
       <div
-        class="md-layout-item md-medium-size-50 md-xsmall-size-100 md-size-33"
+        class="x md-layout-item md-medium-size-50 md-xsmall-size-100 md-size-30"
+        style="margin-left: 50px;"
       >
         <stats-card data-background-color="red">
           <template slot="header">
             <md-icon>content_copy</md-icon>
           </template>
 
-          <template slot="content">
-            <b><p class="category" style="text-color:blue;font-size:20px;">Volatility Index</p></b>
-            <h3 class="title">75</h3>
+           <template slot="content"><br/>
+            <b><p class="category" style="text-color:blue;font-size:40px;">Volatility Index</p></b><br/>
+            <h3 class="title">{{this.volNumber}}</h3>
           </template>
 
         </stats-card>
@@ -100,6 +78,8 @@
 </template>
 
 <script>
+import Chart from 'chart.js'
+
 import {
   StatsCard,
   ChartCard,
@@ -108,16 +88,22 @@ import {
 } from "@/components";
 
 export default {
+  mounted() {
+
+  },
   components: {
     StatsCard,
     ChartCard,
   },
   data() {
     return {
+      riskNumber: 6,
+      confidenceNumber: 7,
+      volNumber: 2,
       dailySalesChart: {
         data: {
-          labels: ["M", "T", "W", "T", "F", "S", "S"],
-          series: [[12, 17, 7, 17, 23, 18, 38]],
+          labels: ["M", "T", "W", "T", "F", "S", "S", "M", "T", "W", "T", "F", "S", "S"],
+          series: [[12, 17, 7, 17, 23, 18, 38, 12, 17, 7, 17, 23, 18, 38]],
         },
         options: {
           lineSmooth: this.$Chartist.Interpolation.cardinal({
@@ -136,5 +122,42 @@ export default {
       
     };
   },
+  methods: {
+    oneweek() {
+      this.riskNumber += 7;
+      
+
+    }
+  },
 };
 </script>
+
+<style>
+.xi {
+  width: 40em;
+}
+#title {
+  font-size:50px;
+}
+
+.title {
+  font-size: 38px;
+}
+
+.bt {
+  color: blue;
+  font-size: 35px;
+  margin-left: 28px;
+}
+
+html {
+  overflow-x:hidden;
+}
+.x !important {
+  height:300vh;
+}
+
+.ct-chart {
+  height:40vh;
+}
+</style>
--- training.py
+++ training.py
@@ -1,15 +1,73 @@
 #Dependencies
+import pickle
+import numpy
 import keras
 from keras.models import Sequential
 from keras.layers import Dense
 
-# Neural network
-model = Sequential()
-model.add(Dense(64, input_dim=31, activation='relu'))
-model.add(Dense(64, activation='relu'))
-model.add(Dense(32, activation='relu'))
-model.add(Dense(30, activation='sigmoid'))
 
-model.compile(loss='mse', optimizer='adam', metrics=['accuracy'])
-history = model.fit(x_train, mse_train, epochs=100, batch_size=256)
+filehandler = open("filename_pi.obj", 'rb') 
+object = pickle.load(filehandler)
+object = object[0]
 
+input_premiums = [x for x in range(92, 122, 1)]
+expected_inps = []
+expected = []
+
+prices = object[0]
+probs = object[1]
+
+prev_diff = 10000000
+prev_val = 0
+current_index = 0
+for price in prices:
+	if current_index == len(input_premiums):
+		break
+	diff = input_premiums[current_index] - price
+	if diff < 0:
+		expected_inps.append(prev_val)
+		current_index += 1
+	prev_diff = diff
+	prev_val = price
+
+x = zip(prices, probs)
+counter = 0
+for i in x:
+	if counter == len(expected_inps):
+		break
+	if i[0] == expected_inps[counter]:
+		expected.append(i[1])
+		counter += 1
+
+if len(input_premiums) is not len(expected):
+	print("fudck wrong lens")
+
+input_train = input_premiums #[exp]
+expected_train = expected
+
+classifier = Sequential()
+    
+# Adding the input layer and the first hidden layer
+classifier.add(Dense(units = 31, activation = 'relu'))
+    
+# Adding the second hidden layer
+classifier.add(Dense(units = 64, activation = 'relu'))
+classifier.add(Dense(units = 64, activation = 'relu'))
+
+# Adding the output layer
+classifier.add(Dense(units = 30, activation = 'sigmoid'))
+    
+# Compiling the ANN
+classifier.compile(loss='mean_absolute_error', optimizer='adam', metrics=['mean_absolute_error'])
+
+History = classifier.fit(x = input_train, y = expected_train, batch_size = 31, epochs = 150, verbose = 1)
+
+# print(numpy.array(input_train))
+# # # Neural network
+# model = Sequential()
+# model.add(Dense(64, input_dim=30, activation='relu'))
+# model.add(Dense(64, activation='relu'))
+# model.add(Dense(32, activation='relu'))
+# model.add(Dense(30, activation='sigmoid'))
+# model.compile(loss='mse', optimizer='adam', metrics=['accuracy'])
+# history = model.fit(numpy.asarray(input_train), numpy.asarray(expected_train), epochs=100, batch_size=30, verbose=1)
